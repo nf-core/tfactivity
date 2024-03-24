7a0615c8292f984acb4bbfbd66174b684ace4504
Nico Trummer <nictru32@gmail.com>
2024-03-24 11:09:30 +0100

    Implement dummy ranking

--- modules/local/report/create/templates/build.py
+++ modules/local/report/create/templates/build.py
@@ -27,13 +27,36 @@ text = "Hello, World!"
 tf = env.get_template("tf.html")
 tg = env.get_template("tg.html")
 snp = env.get_template("snp.html")
+styles = env.get_template("styles.css")
+
+ranking = {
+    "TF1": {
+        "A": 1,
+        "B": 2,
+    },
+    "TF2": {
+        "A": 2,
+        "B": 1,
+    },
+    "TF3": {
+        "A": 3,
+    }
+}
+
+assay_sizes = {
+    "A": 3,
+    "B": 3,
+}
 
 os.makedirs(out_dir, exist_ok=True)
 with open(os.path.join(out_dir, "index.html"), "w") as f:
-    f.write(tf.render())
+    f.write(tf.render(ranking=ranking, assay_sizes=assay_sizes))
 
 with open(os.path.join(out_dir, "target_genes.html"), "w") as f:
     f.write(tg.render())
 
 with open(os.path.join(out_dir, "snps.html"), "w") as f:
     f.write(snp.render())
+
+with open(os.path.join(out_dir, "styles.css"), "w") as f:
+    f.write(styles.render())
