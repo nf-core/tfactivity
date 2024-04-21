1a45d89e817e2d77e07d38c0510494ccaab63e78
Nico Trummer <nictru32@gmail.com>
2024-04-21 14:52:40 +0200

    Use module in ranking

--- modules/local/ranking/ranking/main.nf
+++ modules/local/ranking/ranking/main.nf
@@ -18,14 +18,5 @@ process RANKING {
     path  "versions.yml"                     , emit: versions
 
     script:
-    """
-    ranking.py --input ${tf_tg_score} --alpha ${alpha} --out_tfs ${meta.id}.tf_ranking.tsv --out_tgs ${meta.id}.tg_ranking.tsv
-
-    cat <<-END_VERSIONS > versions.yml
-    "${task.process}":
-        python: \$(python --version | sed 's/Python //g')
-        pandas: \$(python -c "import pandas; print(pandas.__version__)")
-        numpy: \$(python -c "import numpy; print(numpy.__version__)")
-    END_VERSIONS
-    """
+    template "ranking.py"
 }
--- modules/local/ranking/ranking/templates/ranking.py
+++ modules/local/ranking/ranking/templates/ranking.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
-import argparse
 import pandas as pd
 import statistics as st
+import scipy
 import scipy.stats as stats
+import platform
 
-parser = argparse.ArgumentParser(description='Create TF ranking')
-parser.add_argument('--input', type=str, help='Score file', required=True)
-parser.add_argument('--alpha', type=float, help='Alpha value', required=True)
-parser.add_argument('--out_tfs', type=str, help='Output tf file', required=True)
-parser.add_argument('--out_tgs', type=str, help='Output gene file', required=True)
+def format_yaml_like(data: dict, indent: int = 0) -> str:
+    """Formats a dictionary to a YAML-like string.
 
-args = parser.parse_args()
+    Args:
+        data (dict): The dictionary to format.
+        indent (int): The current indentation level.
 
-df_genes = pd.read_csv(args.input, sep='\t', header=0, index_col=0)
+    Returns:
+        str: A string formatted as YAML.
+    """
+    yaml_str = ""
+    for key, value in data.items():
+        spaces = "  " * indent
+        if isinstance(value, dict):
+            yaml_str += f"{spaces}{key}:\\n{format_yaml_like(value, indent + 1)}"
+        else:
+            yaml_str += f"{spaces}{key}: {value}\\n"
+    return yaml_str
+
+df_genes = pd.read_csv("$tf_tg_score".replace("\\\\", ""), sep='\\t', header=0, index_col=0)
 
 # Save whole content of the dataframe in a single, flattened list
 background = df_genes.values.flatten().tolist()
@@ -24,7 +36,6 @@ def mann_whitney_u(background, foreground):
     return p
 
 df_ranking = pd.DataFrame(columns=['sum', 'mean', 'q95', 'q99', 'median', 'p-value'])
-# Transform df to have the following columns: sum, mean, q95, q99, median, p-value
 df_ranking['sum'] = df_genes.sum()
 df_ranking['mean'] = df_genes.mean()
 df_ranking['q95'] = df_genes.quantile(0.95)
@@ -32,7 +43,7 @@ df_ranking['q99'] = df_genes.quantile(0.99)
 df_ranking['median'] = df_genes.median()
 df_ranking['p-value'] = df_genes.apply(lambda x: mann_whitney_u(background, x))
 
-df_ranking = df_ranking[(df_ranking['median'] > background_median) & (df_ranking['p-value'] < args.alpha)]
+df_ranking = df_ranking[(df_ranking['median'] > background_median) & (df_ranking['p-value'] < float("$alpha"))]
 
 df_ranking.sort_values(by=['median'], ascending=False, inplace=True)
 
@@ -41,7 +52,7 @@ df_ranking['rank'] = range(1, length + 1)
 df_ranking['dcg'] = 1 - (df_ranking['rank'] - 1) / length
 
 df_ranking = df_ranking[['dcg']]
-df_ranking.to_csv(args.out_tfs, sep='\t')
+df_ranking.to_csv("${meta.id}.tf_ranking.tsv", sep='\\t')
 
 # Save gene-wise DCGs per TF
 significant_tfs = df_ranking.index
@@ -49,4 +60,17 @@ df_genes = df_genes[significant_tfs]
 
 # Calculate gene-wise DCGs per TF
 df_genes = 1 - (df_genes.rank(ascending=False).astype(int) / len(df_genes.index))
-df_genes.to_csv(args.out_tgs, sep='\t')
+df_genes.to_csv("${meta.id}.tg_ranking.tsv", sep='\\t')
+
+
+# Create version file
+versions = {
+    "${task.process}" : {
+        "python": platform.python_version(),
+        "pandas": pd.__version__,
+        "scipy": scipy.__version__
+    }
+}
+
+with open("versions.yml", "w") as f:
+    f.write(format_yaml_like(versions))
