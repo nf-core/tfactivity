49f2acbc37d077198771a71796e2053dbd3aa93a
Leon Hafner <leon.hafner@tum.de>
2024-09-13 12:30:07 +0200

    Use nf-core/chipseq dev revision

--- assets/end_to_end.py
+++ assets/end_to_end.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import string
 import pandas as pd
 
 parser = argparse.ArgumentParser()
@@ -9,6 +10,7 @@ parser.add_argument('--outdir', type=str, required=False, help='Path to output d
 parser.add_argument('--fasta', type=str, required=False, help='Path to the FASTA file')
 parser.add_argument('--gtf', type=str, required=False, help='Path to the GTF file')
 parser.add_argument('--apptainer_cache', type=str, required=False, help='Path to the apptainer cache directory')
+parser.add_argument('--process_executor', type=str, required=False, help='Executor for the nextflow pipelines', default='local')
 
 parser.add_argument('--rna_seq', type=str, required=False, help='RNA-Seq directory')
 parser.add_argument('--chip_seq', type=str, required=False, help='ChIP-Seq directory')
@@ -21,22 +23,25 @@ outdir = args.outdir
 fasta = args.fasta
 gtf = args.gtf
 apptainer_cache = args.apptainer_cache
+process_executor = args.process_executor
 
 rna_seq = args.rna_seq
 chip_seq = args.chip_seq
 atac_seq = args.atac_seq
 
+# TODO: Remove hard coded parameters
 fasta = "https://raw.githubusercontent.com/nf-core/test-datasets/rnaseq/reference/genome.fa"
 gtf = "https://raw.githubusercontent.com/nf-core/test-datasets/rnaseq/reference/genes.gtf"
 rna_seq = "/nfs/home/students/l.hafner/inspect/tfactivity/dev/testing/end_to_end/data/rna_seq"
 chip_seq = "/nfs/home/students/l.hafner/inspect/tfactivity/dev/testing/end_to_end/data/chip_seq"
 outdir = "/nfs/home/students/l.hafner/inspect/tfactivity/dev/testing/end_to_end/output"
 apptainer_cache = "/nfs/scratch/apptainer_cache"
+process_executor = 'local'
 
 os.environ["NXF_APPTAINER_CACHEDIR"] = apptainer_cache
 
 
-# Start only if output directory does not exist
+# Create output directory if not exisiting
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
@@ -47,7 +52,6 @@ def list_files_recursive(directory):
         for file in files:
             file_list.append(os.path.join(root, file))
     return file_list
-
 
 
 # Run nf-core/rnaseq pipeline
@@ -71,7 +75,10 @@ for file_path in file_paths:
     sample_name = "_".join([condition, sample, rep])
     
     if sample_name not in samples:
-        samples[sample_name] = {'sample': sample_name, 'fastq_1': '', 'fastq_2': '', 'strandedness': 'auto'}
+        samples[sample_name] = {'sample': sample_name,
+                                'fastq_1': '',
+                                'fastq_2': '',
+                                'strandedness': 'auto'}
     
     if read == 'R1':
         samples[sample_name]['fastq_1'] = file_path
@@ -90,6 +97,7 @@ nextflow run \
     --igenomes_ignore \
     --genome null \
     -profile {profile} \
+    -process.executor {process_executor} \
     -resume \
     --skip_deseq2_qc true
 """
@@ -100,7 +108,7 @@ os.system(rnaseq_run)
 # Run nfcore/chipseq pipeline
 path_nfcore_chipseq = os.path.join(outdir, 'nfcore-chipseq')
 path_outdir_chipseq = os.path.join(path_nfcore_chipseq, 'output')
-path_samplesheet_chipseq = os.path.join(path_nfcore_chipseq, 'samplesheet_chipeq.csv')
+path_samplesheet_chipseq = os.path.join(path_nfcore_chipseq, 'samplesheet_chipseq.csv')
 
 if not os.path.exists(path_nfcore_chipseq):
     os.makedirs(path_nfcore_chipseq)
@@ -116,30 +124,51 @@ for file_path in file_paths:
     # Remove .fq.gz and split file name
     sample, rep, read = basename.split('.')[0].split('_')
 
-    sample_name = "_".join([condition, antibody, sample, rep])
+    condition_antibody_sample = "_".join([condition, antibody, sample])
+
+    sample_rep = '_'.join([condition_antibody_sample, rep])
     
-    if sample_name not in samples:
-        samples[sample_name] = {'sample': sample_name, 'fastq_1': '', 'fastq_2': '', 'antibody': antibody, 'control': f'{condition}_CONTROL_{sample}_{rep}'}
+    # Strip 'REP' from replicate
+    if sample_rep not in samples:
+        samples[sample_rep] = {'sample': condition_antibody_sample,
+                               'fastq_1': '',
+                               'fastq_2': '',
+                               'replicate': rep.strip(string.ascii_letters),
+                               'antibody': antibody,
+                               'control': f'{condition}_CONTROL_{sample}',
+                               'control_replicate': rep.strip(string.ascii_letters)}
     
     if read == 'R1':
-        samples[sample_name]['fastq_1'] = file_path
+        samples[sample_rep]['fastq_1'] = file_path
     elif read == 'R2':
-        samples[sample_name]['fastq_2'] = file_path
+        samples[sample_rep]['fastq_2'] = file_path
 
 chipseq_samplesheet = pd.DataFrame.from_dict(samples, orient='index')
+
+# Remove control files that do not exist
 chipseq_samplesheet['control'] = chipseq_samplesheet['control'].apply(lambda x: x if x in chipseq_samplesheet['sample'].values else '')
+
+# Remove antibody, control and control_replicate column from dataframe
+chipseq_samplesheet.loc[chipseq_samplesheet['sample'].str.contains('CONTROL'), ['antibody', 'control', 'control_replicate']] = ''
 chipseq_samplesheet.to_csv(path_samplesheet_chipseq, index=False)
 
+# Runs on revision dev as the pipeline has not had an release for some time
+# TODO change parameter read_length
+# TODO change parameter skip_preseq
+# TODO disable processes not needed for downstream pipelines
 chipseq_run = f"""
 nextflow run \
     nf-core/chipseq \
+    -r dev \
     --input {path_samplesheet_chipseq} \
     --outdir {path_outdir_chipseq} \
     --gtf {gtf} \
     --fasta {fasta} \
-    --read_length 150 \
-    -profile docker \
-    -resume
+    --read_length 50 \
+    -profile {profile} \
+    -process.executor {process_executor} \
+    -resume \
+    --skip_preseq true
 """
 
 os.chdir(path_nfcore_chipseq)
